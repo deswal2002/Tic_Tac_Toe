321d5f729551a11cda6ae84094b07ad30abedae5
Monish Deswal <monishdeswal2002@gmail.com>
2024-01-09 16:05:20 +0530

    version 1

--- src/Play_game.jsx
+++ src/Play_game.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './Tic_tac_toe.css'
 import { useLocation, useNavigate } from "react-router-dom"
+import blue_cross from './assets/blue_cross.svg'
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Dangrek&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 </style>
@@ -172,7 +173,7 @@ function Play_game() {
         <>
             <div className='box'>
                 <div id='box2' >
-                    <img src='../../public/blue_cross.svg ' id='blue_cross_game' />
+                    <img src={blue_cross} id='blue_cross_game' />
                     <img src='../../public/yellow_circle.svg' id='yellow_circle_game' />
                     <div id='turn'>
                         <div id='play_chose'>{data === 1 ? 'X' : 'O'}</div>
